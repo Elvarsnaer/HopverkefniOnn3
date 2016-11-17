76546235db868bf41655ed344b6f8a32deca4910
cindexin <arnardadi99@hotmail.com>
2016-11-17 10:53:26 +0000

    Byrjun Verk

---
+++ Hopverkefni/Hopverkefni/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hopverkefni
+{
+    /*
+     * Hópverkefni Önn 3
+     * Arnar Daði og Elvar Snær
+     * Byjrað 17.11.16
+     */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
